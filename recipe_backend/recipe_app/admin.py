3e21c1ee3a043b26a0b37bfb8af03d5e1183297b
Jocelyn Tandrea <jta189@sfu.ca>
2022-01-15 15:52:31 -0800

    Create model for Food and Recipe

--- recipe_backend/recipe_app/admin.py
+++ recipe_backend/recipe_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Food, Recipe
 
 # Register your models here.
+admin.site.register(Food)
+admin.site.register(Recipe)
--- recipe_backend/recipe_app/models.py
+++ recipe_backend/recipe_app/models.py
@@ -1,3 +1,20 @@
+from dataclasses import field
 from django.db import models
 
 # Create your models here.
+class Food(models.Model):
+    class Meta:
+        unique_together = (('name', 'datePurchased'),)
+
+    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=20)
+    datePurchased = models.DateField()
+    daysBeforeExpire = models.IntegerField()
+
+    def __str_(self):
+        return self.name + ", bought" + self.datePurchased
+
+class Recipe(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=100)
+    imageUrl = models.CharField(max_length=200)
