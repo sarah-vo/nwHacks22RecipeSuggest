a5d509ebe4edae27e0abc4ef2dbbe720247efd69
yongqix <yongqi_xu@sfu.ca>
2022-01-15 12:19:12 -0800

    Initial Commit

---
+++ Fridge/FridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridgeApp.swift
+//  Fridge
+//
+//  Created by Yongqi Xu on 2022-01-15.
+//
+
+import SwiftUI
+
+@main
+struct FridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
