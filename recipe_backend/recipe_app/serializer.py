3b6b97f21815d0e94c41556fd6d82ad261e38b54
Sarah Vo <srvo@gmail.com>
2022-01-15 17:10:48 -0800

    serializer bug pls see

---
+++ recipe_backend/recipe_app/serializer.py
@@ -0,0 +1,12 @@
+from .models import Food, Recipe
+from rest_framework import serializers
+
+class FoodSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Food,
+        fields = ['name', 'type', 'datePurchased', 'daysBeforeExpire' ]
+
+# class RecipeSerializers(serializers.ModelSerializer):
+#     class Meta:
+#         model = Recipe,
+#         fields = ['id', 'name', 'imageUrl']
--- recipe_backend/recipe_app/views.py
+++ recipe_backend/recipe_app/views.py
@@ -1,3 +1,12 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+
+from .models import Food, Recipe
+from .serializer import FoodSerializers
 
 # Create your views here.
+class FoodViewSet(viewsets.ModelViewSet):
+    queryset = Food.objects.all(),
+    serializer_class = FoodSerializers
+    permisison_classes = [permissions.IsAuthenticated]
